2254026415af4217da654794f99400ddaf32195a
arihant-2310 <arihant2310@gmail.com>
2020-06-28 16:04:13 +0530

    adds delete survey route

--- src/survey/survey.repository.ts
+++ src/survey/survey.repository.ts
@@ -1,4 +1,5 @@
-import { EntityRepository, Repository, getRepository } from 'typeorm';
+import { EntityRepository, Repository, getRepository, getConnection } from 'typeorm';
+import { Questions } from "./../questions/entity/questions.entity";
 import { Survey } from './entity/survey.entity';
 import { CreateSurveyDto } from './dto/create-survey.dto';
 import { json } from 'express';
@@ -45,4 +46,45 @@ export class SurveyRepository extends Repository<Survey> {
     }
 
   };
+
+  deleteSurvey = async (survey_id:number) => {
+
+    let result
+    try{
+      const getsurvey = await getRepository(Survey)
+      .createQueryBuilder("survey")
+      .where("id = :id", { id: survey_id }).getOne()
+      if(!getsurvey){
+        return{
+          message:"Such a survey doesn't exists!!"
+        }
+      }
+      //this will delete the survey and questions of that particular survey
+      await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(Survey)
+        .where("id = :id", { id: survey_id })
+        .execute()
+        .then(res=>{
+          getConnection()
+          .createQueryBuilder()
+          .delete()
+          .from(Questions)
+          .where("survey_id = :id", { id: survey_id })
+          .execute()
+      })
+
+        return {
+        statusCode:201,
+        message:"Survey Successfully Deleted!!"
+        }
+
+    }catch(err){
+      console.log(err)
+      return {
+        error:"Couldn't perform delete survey operation!!"
+      }
+    }
+  }
 }
